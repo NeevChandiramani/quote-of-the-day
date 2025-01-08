a4c99464f2c7ee69a2c44a6dbbb0dbc32287c5e3
Neev Chandiramani <102186419+NeevChandiramani@users.noreply.github.com>
2025-01-08 12:14:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 async function fetchQuote() {
     try {
-        const response = await fetch("https://api.quotable.io/quotes/random");
+        const response = await fetch("https://api.quotable.io/random");
         if (!response.ok) {
             throw new Error(`Failed to fetch quote: ${response.statusText}`);
         }
-        const [data] = await response.json(); // API returns an array with one quote
+        const data = await response.json();
         return {
             content: data.content,
             author: data.author
